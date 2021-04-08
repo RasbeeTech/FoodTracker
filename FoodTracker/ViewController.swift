8c889bdf3549cbdf954fbb0716dc9d0cdedb397b
MykalB081 <mykalb081@gmail.com>
2021-04-08 10:22:03 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by RasB on 2021-04-08.
+//  Copyright © 2021 Mykal Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
