f695d40b30567e7f0dac3b851c5563114eb6f3fa
MykalB081 <mykalb081@gmail.com>
2021-04-09 12:21:01 -0400

    Create Table View
    - custom UITableViewCell
    - create a subclass of UITableViewController
    - sample images added
    - name change: ViewController -> MealViewController
    - Some code cleanup

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by RasB on 2021-04-09.
+//  Copyright © 2021 Mykal Bailey. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by RasB on 2021-04-08.
@@ -8,10 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -27,7 +26,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         return true
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
+        
     }
     
     //MARK: Actions
