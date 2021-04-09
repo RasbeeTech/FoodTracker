e127f289cb2633cf27cad4f4ed6aadff8e6e3150
MykalB081 <mykalb081@gmail.com>
2021-04-09 18:58:34 -0400

    Persistent Data
    - implement NSCoding and NSObject

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    //MARK: Types
+
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     // Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
@@ -31,5 +40,18 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
     }
 }
