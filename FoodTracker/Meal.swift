11775719f2b43de2c982b6ad5d54c27b4776704d
MykalB081 <mykalb081@gmail.com>
2021-04-11 09:34:08 -0400

    Revert "persisten data"
    This reverts commit 44b1e9235d9bce09f3285b364ac368a80d5348bf.

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -17,11 +17,6 @@ class Meal: NSObject, NSCoding {
     var photo: UIImage?
     var rating: Int
     
-    //MARK: Archiving Paths
-    
-    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
-    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
-    
     //MARK: Types
 
     struct PropertyKey {
@@ -46,7 +41,6 @@ class Meal: NSObject, NSCoding {
         self.photo = photo
         self.rating = rating
     }
-    
     //MARK: NSCoding
     func encode(with aCoder: NSCoder) {
         aCoder.encode(name, forKey: PropertyKey.name)
@@ -54,18 +48,10 @@ class Meal: NSObject, NSCoding {
         aCoder.encode(rating, forKey: PropertyKey.rating)
     }
     required convenience init?(coder aDecoder: NSCoder) {
-        
         // The name is required. If we cannot decode a name string, the initializer should fail.
         guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
             os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
             return nil
         }
-        
-        // Because photo is an optional property of Meal, just use conditional cast.
-        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
-        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
-        
-        // Must call designated initializer.
-        self.init(name: name, photo: photo, rating: rating)
     }
 }
